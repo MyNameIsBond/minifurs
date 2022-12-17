446a10d3cc94cb96f0cb673571d60234953b9df9
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-12-17 18:36:29 +0000

    cc

--- components/home/CardDisplaySceleton.tsx
+++ components/home/CardDisplaySceleton.tsx
@@ -4,12 +4,12 @@ import { ArrowLongRightIcon } from "react-native-heroicons/outline";
 
 export default function CardDisplaySceleton({ title, children }) {
   return (
-    <View className="">
+    <View>
       <View className="flex-row justify-between px-4">
         <Text className="capitalize font-bold text-xl">{title}</Text>
         <View className="flex-row items-center gap-x-2">
           <Text className="text-accent-orange">See All</Text>
-          <ArrowLongRightIcon className="" color="#E68314" />
+          <ArrowLongRightIcon color="#E68314" />
         </View>
       </View>
       <View>{children}</View>
