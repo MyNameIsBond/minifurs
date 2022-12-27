28245c0e007f6fd78629c6cb9d2812f2e990c502
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-12-27 20:05:13 +0000

    cc

---
+++ components/AdBanner.tsx
@@ -0,0 +1,43 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function AdBanner({
+  title,
+  subtitle,
+  image,
+}: {
+  title: string;
+  subtitle: string;
+  image: string;
+}) {
+  return (
+    <View className="px-4">
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        colors={["#EFE5D5", "#F2F0E0"]}
+        className="items-center flex-row p-4 rounded-xl my-10 w-full"
+      >
+        <>
+          <View>
+            <Text className="text-base font-black max-w-xs text-accent-orange">
+              {title}
+            </Text>
+            <Text className="text-accent-orange pt-2 text-sm max-w-[180]">
+              {subtitle}
+            </Text>
+            <Text className="text-sm max-w-[180] pt-3 text-accent-green">
+              Coming soon...
+            </Text>
+          </View>
+          <Image
+            style={{ height: 150, width: 150 }}
+            resizeMode="contain"
+            source={require("../assets/vr_headset.png")}
+          />
+        </>
+      </LinearGradient>
+    </View>
+  );
+}
--- components/home/HomeBody.tsx
+++ components/home/HomeBody.tsx
@@ -1,3 +1,4 @@
+import AdBanner from "../AdBanner";
 import CardDisplaySceleton from "./CardDisplaySceleton";
 import HomeCard from "./HomeCard";
 
@@ -7,10 +8,22 @@ export default function HomeBody({
   products: any[] | null;
 }): JSX.Element {
   return (
-    <CardDisplaySceleton title="latest release">
-      {products.map((product) => (
-        <HomeCard key={product.id} product={product} />
-      ))}
-    </CardDisplaySceleton>
+    <>
+      <CardDisplaySceleton title="latest release">
+        {products.map((product) => (
+          <HomeCard key={product.id} product={product} />
+        ))}
+      </CardDisplaySceleton>
+      <AdBanner
+        image="https://scashksznwiivlpgkftm.supabase.co/storage/v1/object/public/minifur/appImages/vr_headset.png"
+        title="Virtual Reality Showroom"
+        subtitle="Allows you to view our showrooms containing our latest furniture collections"
+      />
+      <CardDisplaySceleton title="latest release">
+        {products.slice(0, 2).map((product) => (
+          <HomeCard key={product.id} product={product} />
+        ))}
+      </CardDisplaySceleton>
+    </>
   );
 }
