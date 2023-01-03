d26f0ed97a006d7cb370ca4fdbe651e7520f731e
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2023-01-03 20:03:09 +0000

    cc

--- screens/Favourite.tsx
+++ screens/Favourite.tsx
@@ -1,7 +1,27 @@
 import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+import { useUser } from "../lib/helpers/UserContext";
 
 export default function Favourite({}) {
+  const [favourites, setFavourites] = useState<any[] | null>([]);
+
+  const fetchFavourites = async () => {
+    try {
+      const { user } = useUser();
+      const { data, error } = await supabase
+        .from("favourites")
+        .select("*")
+        .eq("user_id", user.id);
+      console.log("DATA:", data);
+      setFavourites(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchFavourites();
+  }, []);
   return (
     <SafeAreaView>
       <View>
